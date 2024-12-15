59617eabbd974120e7ea7645342a1b56ee62e1b9
SergiGilRuiz <sergigilruiz@gmail.com>
2024-12-15 14:11:40 +0100

    Part 1.1 acabada

--- Projecte_final_PAI/primera_part.js
+++ Projecte_final_PAI/primera_part.js
@@ -1,35 +1,48 @@
 function validarNom() {
   const nom = document.getElementById("nom").value.trim();
+  //trim és per eliminar els espais en blanc al principi i al final de valor obtingut (nom)
+
   const errorElement = document.getElementById("error-nom");
-  let errorMessage = "";
+  let errorMissatge = "";
 
   if (nom === "") {
-    errorMessage = "El camp no pot estar buit.";
+    errorMissatge = "El camp no pot estar buit.";
   } else {
-    // Separar les paraules
-    const paraules = nom.split(" ");
-    for (let paraula of paraules) {
-      // Comprovar que no hi hagi números
-      if (/\d/.test(paraula)) {
-        errorMessage = "El nom no pot contenir números.";
-        break;
-      }
-
+      // Dividieix el text introduït per l'usuari (nom) en un array de paraules, usant els espais (" ") com a separadors.
+      const paraules = nom.split(" ");
+      // Cada paraula
+      for (let paraula of paraules) {
+          // Cada caràcter de la paraula
+          for (let i = 0; i < paraula.length; i++) {
+              //Guardem cada caràcter a la variable caràcter
+              const caracter = paraula[i];
+              if (caracter >= '0' && caracter <= '9') {
+                  errorMissatge = "El nom no pot contenir números.";
+                  break;
+              }
+          }
+          // Si s'ha avisat de l'error, parem el bucle
+          if (errorMissatge) {
+              break;
+          }
+  
       // Comprovar que la primera lletra sigui majúscula
+      //.charAt serveix per accedir a un caràcter individual
       const primeraLletra = paraula.charAt(0);
+      //.slice serveix per agafar una part d'una cadena de caràcters, si nomes poses 1 número, agafa a partir d'aquell fins al final
       const restaParaula = paraula.slice(1);
 
       if (primeraLletra !== primeraLletra.toUpperCase() || 
           restaParaula !== restaParaula.toLowerCase()) {
-        errorMessage = "Cada paraula ha de començar amb majúscula.";
+          errorMissatge = "El nom i el cognom ha de começar per amb majúscula.";
         break;
       }
     }
   }
 
   // Mostrar el missatge d'error si cal
-  if (errorMessage) {
-    errorElement.textContent = errorMessage;
+  if (errorMissatge) {
+    errorElement.textContent = errorMissatge;   // Assigna el missatge d'error a l'element
   } else {
     errorElement.textContent = ""; // Netejar errors si tot és correcte
   }
