b233c32a3f3265e5db8b91c0c4e4f29d514a04b8
SergiGilRuiz <sergigilruiz@gmail.com>
2024-12-16 11:29:46 +0100

    1.3

--- Projecte_final_PAI/primera_part.js
+++ Projecte_final_PAI/primera_part.js
@@ -1,4 +1,4 @@
-//Nom i cognoms
+// Nom i cognoms
 function validarNom() {
   const nom = document.getElementById("nom").value.trim();
   //trim és per eliminar els espais en blanc al principi i al final de valor obtingut (nom)
@@ -9,11 +9,11 @@ function validarNom() {
   if (nom === "") {
     errorMissatge = "El camp no pot estar buit.";
   } else {
-      // Dividieix el text introduït per l'usuari (nom) en un array de paraules, usant els espais (" ") com a separadors.
+      //Dividieix el text introduït per l'usuari (nom) en un array de paraules, usant els espais (" ") com a separadors.
       const paraules = nom.split(" ");
-      // Cada paraula
+      //Cada paraula
       for (let paraula of paraules) {
-          // Cada caràcter de la paraula
+          //Cada caràcter de la paraula
           for (let i = 0; i < paraula.length; i++) {
               //Guardem cada caràcter a la variable caràcter
               const caracter = paraula[i];
@@ -22,12 +22,12 @@ function validarNom() {
                   break;
               }
           }
-          // Si s'ha avisat de l'error, parem el bucle
+          //Si s'ha avisat de l'error, parem el bucle
           if (errorMissatge) {
               break;
           }
   
-      // Comprovar que la primera lletra sigui majúscula
+      //Comprovar que la primera lletra sigui majúscula
       //.charAt serveix per accedir a un caràcter individual
       const primeraLletra = paraula.charAt(0);
       //.slice serveix per agafar una part d'una cadena de caràcters, si nomes poses 1 número, agafa a partir d'aquell fins al final
@@ -41,23 +41,23 @@ function validarNom() {
     }
   }
 
-  // Mostrar el missatge d'error si cal
+  //Mostra el missatge d'error si cal
   if (errorMissatge) {
-    errorElement.textContent = errorMissatge;   // Assigna el missatge d'error a l'element
+    errorElement.textContent = errorMissatge;   //Assigna el missatge d'error a l'element
   } else {
-    errorElement.textContent = ""; // Netejar errors si tot és correcte
+    errorElement.textContent = ""; //Netejar errors si tot és correcte
   }
 }
 
 
 
-//Rang d'edats
-// Validació del camp 'Rang d'edats'
+// Rang d'edats
+//Validació del camp 'Rang d'edats'
 function validateEdat() {
   const selectEdat = document.getElementById('edat');
   const errorElement = document.getElementById('error-edat');
   
-  // Comproveu si heu seleccionat una opció vàlida
+  //Comprovem si s'ha seleccionat una opció vàlida
   if (selectEdat.value === "") {    //És el valor que te l'opció "Selecciona una opció"
       errorElement.textContent = "Per favor, selecciona una opció vàlida.";
   } else {
@@ -65,6 +65,36 @@ function validateEdat() {
   }
 }
 
+
+
+// Codi postal
+const inputCodiPostal = document.getElementById("codi-postal");
+const errorCodi = document.getElementById("error-codi");
+
+//Validem el codi postal
+function validarCodiPostal() {
+    const valor = inputCodiPostal.value;
+    let mensajeError = "";
+
+    //Comprovem que tots els caràcters son números
+    for (let i = 0; i < valor.length; i++) {
+      const car = valor[i];
+      //En comptes de dir la situació on no salta l'error, diem que si el caràcter és més petit que 0 o més gran a 9, salta l'error
+      //Això serveix també per les lletres, tot el que no sigui un número del 0 al 9, salta l'error
+      if (car < '0' || car > '9') { 
+          mensajeError = "El codi postal només pot contenir números del 0 al 9.";
+          break;
+      }
+  }
+
+    //Al html ja tenim un maxlength que no put superar els 5 caràcters però algú podria escriure menys de 5
+    if (valor.length !== 5) {
+        mensajeError = "El codi postal ha de tenir exactament 5 números.";
+    }
+
+    //Mostra el missatge d'error si és necessari
+    errorCodi.textContent = mensajeError;
+}
 
 
 
